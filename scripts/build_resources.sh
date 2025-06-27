15b246b57c7b4f2e03b09f0b90112f7134b10558
Tasha Snow <tsnow@mines.edu>
2025-06-27 13:28:25 -0600

    Create build_resources script

---
+++ scripts/build_resources.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+cd "${BASH_SOURCE%/*}/" || exit
+
+if [ -d "../book/_build/html/assets" ]; then
+   rm -rf ../_build/html/assets
+   echo "Removed jupyterbook assets"
+fi
+
+check_success() {
+  if [[ $? -ne 0 ]]; then
+    printf "\033[1;31m ERROR \033[0m\n"
+    exit 1
+  else
+    printf "\033[1;32m SUCCESS \033[0m\n"
+  fi
+}
+
+echo "Building the Jupyter Book"
+cd ../
+myst build --html
+
+check_success
